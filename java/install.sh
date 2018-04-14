dca6e3dc0d1cc381a0c71c1546fd389675cdde12
Avi Cavale <avi@shippable.com>
2018-04-14 08:47:25 -0700

    initial commit

--- java/install.sh
+++ java/install.sh
@@ -7,7 +7,7 @@ apt-get install -q -y default-jdk=2:1.8*
 echo "================= Installing openjdk-8-jdk ==================="
 add-apt-repository -y ppa:openjdk-r/ppa
 apt-get update
-apt-get install -q -y openjdk-8-jdk=8u151*
+apt-get install -q -y openjdk-8-jdk
 update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
 update-alternatives --set javac /usr/lib/jvm/java-8-openjdk-amd64/bin/javac
 add-apt-repository ppa:maarten-fonville/ppa
@@ -19,7 +19,7 @@ echo "================ Installing oracle-java8-installer ================="
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
 add-apt-repository -y ppa:webupd8team/java
 apt-get update
-apt-get install -y -q oracle-java8-installer=8u161*
+apt-get install -y -q oracle-java8-installer
 update-alternatives --set java /usr/lib/jvm/java-8-oracle/jre/bin/java
 update-alternatives --set javac /usr/lib/jvm/java-8-oracle/bin/javac
 update-alternatives --set javaws /usr/lib/jvm/java-8-oracle/jre/bin/javaws
